7bb1a6a9cf51ba1bbed9998f502cf7096caa73af
Prajwal vasishta <prajwal@Prajwals-MacBook-Pro.local>
2023-06-04 20:10:54 +0530

    Names.txt file modified

---
+++ Demo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Prajwal vasishta on 04/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
